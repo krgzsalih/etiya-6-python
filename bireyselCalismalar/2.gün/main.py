097a95604a863728eea7ea9636cf75fb10c7b8fa
krgzsalih <krgzahmetsalih@gmail.com>
2023-04-06 14:43:26 +0300

    2. gün bireysel çalışma eklendi

---
+++ bireyselCalismalar/2.gün/main.py
@@ -0,0 +1,20 @@
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6)
+
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
+
+if (final < 40):
+    print("Final 40'ın altında olduğu için kaldınız")
+elif (ortalama < 50):
+    print("Ortalama 50 altında olduğundan dolayı kaldınız")
+elif (vize == final * 2):
+    print("Kaldı")
+else:
+    print("Geçti")
+
+
+# karar yapıları -end
