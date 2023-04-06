87856478031159382c63f3ff980cadf64479dc31
krgzsalih <krgzahmetsalih@gmail.com>
2023-04-06 15:03:24 +0300

    added logic operators

--- courseCodes/main.py
+++ courseCodes/main.py
@@ -7,14 +7,18 @@ ortalama = (vize * 0.4) + (final * 0.6)
 # eğer vize finalin 2 katı ise kullanıcı kaldı
 # bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
 
-if (final < 40):
-    print("Final 40'ın altında olduğu için kaldınız")
-elif (ortalama < 50):
-    print("Ortalama 50 altında olduğundan dolayı kaldınız")
-elif (vize == final * 2):
+# if (final < 40):
+#     print("Final 40'ın altında olduğu için kaldınız")
+# elif (ortalama < 50):
+#     print("Ortalama 50 altında olduğundan dolayı kaldınız")
+# elif (vize == final * 2):
+#     print("Kaldı")
+# else:
+#     print("Geçti")
+
+# ANOTHER WAY
+if final < 40 or ortalama < 50 or vize == final * 2:
     print("Kaldı")
 else:
     print("Geçti")
-
-
 # karar yapıları -end
