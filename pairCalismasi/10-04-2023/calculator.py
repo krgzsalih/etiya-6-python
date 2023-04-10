6493164469b38b1bc0d031f27fcd8eec0839803b
krgzsalih <krgzahmetsalih@gmail.com>
2023-04-10 15:39:22 +0300

    simple calculator added

---
+++ pairCalismasi/10-04-2023/calculator.py
@@ -0,0 +1,48 @@
+def toplama(a, b):
+    return a + b
+
+
+def cikarma(a, b):
+    return a - b
+
+
+def carpma(a, b):
+    return a * b
+
+
+def bolme(a, b):
+    if (b == 0):
+        return "Bölen sıfır olmamalıdır!!"
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+while True:
+    secim = input(
+        "Lütfen bir işlem seçiniz: ( +, -, *, /, % ) ya da çıkmak için 'q' ya basınız. ")
+    if secim == "q":
+        print("Çıkış yapıldı.")
+        break
+    if secim not in ["+", "-", "*", "/", "%"]:
+        print("Hatalı giriş yaptınız.")
+        continue
+    print(f"Seçilen işlem : {secim}")
+    a = float(input("İlk sayıyı giriniz: "))
+    b = float(input("İkinci sayıyı giriniz: "))
+    if secim == "+":
+        sonuc = toplama(a, b)
+    elif secim == "-":
+        sonuc = cikarma(a, b)
+    elif secim == "*":
+        sonuc = carpma(a, b)
+    elif secim == "/":
+        sonuc = bolme(a, b)
+    elif secim == "%":
+        sonuc = mod(a, b)
+    else:
+        print("Hatalı giriş yaptınız.")
+
+    print(f"Sonuc : {sonuc}")
